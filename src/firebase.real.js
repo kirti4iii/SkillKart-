a422052705f8a4c744f029ff50378aa8024c2603
kirti4iii <kirti4saini@gmail.com>
2025-05-20 16:52:11 +0530

    fixes issue

--- src/firebase.real.js
+++ src/firebase.real.js
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTnxk7k8yBBDuHpQaHt_jZOjVnioZhCdI",
-  authDomain: "skillkart-74898.firebaseapp.com",
-  projectId: "skillkart-74898",
-  storageBucket: "skillkart-74898.firebasestorage.app",
-  messagingSenderId: "786952258619",
-  appId: "1:786952258619:web:13d3e82528bf524c5dc81e",
-  measurementId: "G-0PKQLC4VM9"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
